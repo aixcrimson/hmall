5b7c863b98bdc8c91b9956d31e0c411461440f3a
GotohHitori <hitori150221@outlook.com>
2024-07-17 21:00:53 +0800

    Seata分布式事务-XA和AT模式应用

--- trade-service/src/main/java/com/hmall/trade/service/impl/OrderServiceImpl.java
+++ trade-service/src/main/java/com/hmall/trade/service/impl/OrderServiceImpl.java
@@ -13,9 +13,9 @@ import com.hmall.trade.domain.po.OrderDetail;
 import com.hmall.trade.mapper.OrderMapper;
 import com.hmall.trade.service.IOrderDetailService;
 import com.hmall.trade.service.IOrderService;
+import io.seata.spring.annotation.GlobalTransactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -43,7 +43,7 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
     private final CartClient cartClient;
 
     @Override
-    @Transactional
+    @GlobalTransactional
     public Long createOrder(OrderFormDTO orderFormDTO) {
         // 1.订单数据
         Order order = new Order();
@@ -75,15 +75,15 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
         List<OrderDetail> details = buildDetails(order.getId(), items, itemNumMap);
         detailService.saveBatch(details);
 
-        // 3.扣减库存
+        // 3.清理购物车商品
+        cartClient.deleteCartItemByIds(itemIds);
+
+        // 4.扣减库存
         try {
             itemClient.deductStock(detailDTOS);
         } catch (Exception e) {
             throw new RuntimeException("库存不足！");
         }
-
-        // 4.清理购物车商品
-        cartClient.deleteCartItemByIds(itemIds);
         return order.getId();
     }
 
